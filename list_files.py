00dd42c04f7bdebc6a117e101940d68f34031a82
mkbal <matthew.ballarino@my.jcu.edu.au>
2020-08-19 22:12:00 +1000

    "Testing github"

--- list_files.py
+++ list_files.py
@@ -1,14 +1,17 @@
 """
 Subject: CP1404
-Testing github
+
+Testing github.
 
 Student name: Matthew Ballarino
-Student number: 13291475
+Student number: 1329475
 """
-
 import os
 
 print("The files and folders in {} are:".format(os.getcwd()))
 items = os.listdir('.')
 for item in items:
     print(item)
+
+
+
